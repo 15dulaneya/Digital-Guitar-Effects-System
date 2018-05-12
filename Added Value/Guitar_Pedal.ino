9f2829a0b18b1a1eca041fb8b405cae4b89f731c
15dulaneya <15dulaneya@gmail.com>
2018-05-11 23:15:51 -0400

    Add files via upload

---
+++ Added Value/Guitar_Pedal.ino
@@ -0,0 +1,83 @@
+#include "dsp.h"
+
+// create an array for the delay
+byte array[2000]; 
+
+//define variables
+int j;
+int fx;
+int value50;
+//int value300;
+int value10000;
+int delayed;
+
+int Effect1Pin = 2;
+int Effect2Pin = 7;
+int Effect3Pin = 8;
+
+void setup() {
+  setupIO();
+  pinMode(Effect1Pin,INPUT);
+  pinMode(Effect2Pin,INPUT);
+  pinMode(Effect3Pin,INPUT);
+  
+  //set initial values
+  j = 50;
+  value50 = 50;
+//  value300 = 300;
+  value10000 = 1000;
+  delayed = 0;
+
+  //Serial.begin(9600);
+}
+
+void loop() {
+  
+    //check status of the effect potentiometer and rotary switch
+    readKnobs();
+    //Serial.print(digitalRead(Effect1Pin));
+
+      
+    
+    // Distortion
+      if (digitalRead(Effect1Pin)==1) {
+        value50 = 1 + ((float) fx / (float) 1000);    
+        byte input = analogRead(left);
+        input = (input * value50); 
+        output(left, input); 
+      }
+      
+    // Tremolo
+      if (digitalRead(Effect2Pin)==1) {
+        value10000 = fx * 10;  
+        if(delayed > value10000) { 
+          for(int i = 0; i < 2000; i ++) { // set up a loop    
+            array[i] = array[i] + array[i - 1]; //removes noise and delay
+            output(left, array[i]);
+            array[i] = analogRead(left);
+          }
+          delayed = 0;
+        }
+        delayed++;
+      }
+
+    // Distortion Delay
+      if (digitalRead(Effect3Pin)==1) {
+         for(int i = 0; i < 2000; i ++) { // set up a loop    
+        //array[i] = array[i] + array[i - 1]; //removes noise and some delay
+          output(left, array[i]);
+          array[i] = analogRead(left);
+        }
+      }
+      
+
+
+}
+
+void readKnobs(){
+    
+  //reads the effects pot to adjust
+  //the intensity of the effects above  
+  fx = analogRead(3);
+
+}
---
+++ Added Value/Guitar_Pedal_LCD.ino
@@ -0,0 +1,50 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  pinMode(8,INPUT);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+ // lcd.print("hello, world!");
+ Serial.begin(9600);
+ 
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  //lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  Serial.print(digitalRead(8));
+  if (digitalRead(8)==1) {
+    lcd.setCursor(0,0);
+    lcd.print("Distortion      ");
+    lcd.setCursor(0,1);
+    lcd.print("       ");
+  }
+  else if (digitalRead(7)==1) {
+    lcd.setCursor(0,0);
+    lcd.print("Tremolo         ");
+    lcd.setCursor(0,1);
+    lcd.print("       ");
+  }
+  else if (digitalRead(6)==1) {
+    lcd.setCursor(0,0);
+    lcd.print("Distorted Delay ");
+    lcd.setCursor(0,1);
+    lcd.print("       ");
+  }
+  else {
+    lcd.setCursor(0,0);
+    lcd.print("No Effect       ");
+    lcd.setCursor(0,1);
+    lcd.print("Enabled");
+  //  lcd.display();
+  }
+}
